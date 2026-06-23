f5eb8f78f16b6e44527987c359dfe02d57fc0f7f
LIUZHIYON <2950317571@qq.com>
2026-06-23 13:10:39 +0800

    chore: 清理部署临时脚本

--- deploy/_check_url.py
+++
@@ -1,4 +0,0 @@
-import re
-content = open("C:/Users/29503/Desktop/reminder_codex/robot_websocket/robot_websocket/websocket_client.py", "r", encoding="utf-8").read()
-for m in re.finditer(r"(ws|wss|http)[^\042\047\040\011]*", content):
-    print(m.group())
